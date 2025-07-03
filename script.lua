be045d5f4e8f7c5eb26d18da425ca9ff835712a6
Blackfire <rexmagnusboe@gmail.com>
2025-07-03 09:42:45 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 local placeIds = {
-	[2000151171] = 'scr'; --Build Island 
+	[2000151171] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Build%20Island/yz-script.lua'; --Build Island 
 	[1490130369] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Anomicrazy/script.lua'; --Anomic
 }
 
