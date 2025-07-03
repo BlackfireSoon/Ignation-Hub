b7cb53b60467c14fbe84d8bf14eee594aa74a273
Blackfire <rexmagnusboe@gmail.com>
2025-07-03 09:42:21 +0200

    Update and rename script.luau to script.lua

---
+++ script.lua
@@ -0,0 +1,6 @@
+local placeIds = {
+	[2000151171] = 'scr'; --Build Island 
+	[1490130369] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Anomicrazy/script.lua'; --Anomic
+}
+
+loadstring(placeIds[game.GameId])()
