4664ca2dd49ea12a065d3f5adca60e8e75c5c70e
Blackfire <rexmagnusboe@gmail.com>
2025-07-04 17:29:56 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,7 @@
 local placeIds = {
 	[2000151171] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Build%20Island/yz-script.lua'; --Build Island 
 	[1490130369] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Anomicrazy/script.lua'; --Anomic
-	['else'] = 'https://pastebin.com/raw/tQZYjYV0'; --c00lkidd UI not made by Ignation
 }
-if placeIds[game.GameId] then
-    loadstring(game:HttpGet(tostring(placeIds[game.GameId]))()
-else
-    loadstring(game:HttpGet(placeIds['else']))()
-end
+
+local url = placeIds[game.GameId] or 'https://pastebin.com/raw/tQZYjYV0'
+loadstring(game:HttpGet(url))()
