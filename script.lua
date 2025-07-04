c970514d9907b4572097c13372be3e8fc04eb860
Blackfire <rexmagnusboe@gmail.com>
2025-07-04 17:28:44 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,10 @@
 local placeIds = {
 	[2000151171] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Build%20Island/yz-script.lua'; --Build Island 
 	[1490130369] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Anomicrazy/script.lua'; --Anomic
+	['else'] = 'https://pastebin.com/raw/tQZYjYV0'; --c00lkidd UI not made by Ignation
 }
-
-loadstring(game:HttpGet(placeIds[game.GameId]))()
+if placeIds[game.GameId] then
+    loadstring(game:HttpGet(placeIds[game.GameId]))()
+else
+    loadstring(game:HttpGet(placeIds['else']))()
+end
