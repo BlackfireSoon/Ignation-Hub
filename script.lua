54c40710dd708f6857093854cf47a2bb90657001
Blackfire <rexmagnusboe@gmail.com>
2025-07-20 16:52:57 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,7 @@
 local placeIds = {
-	[2000151171] = {'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Build%20Island/yz-script.lua',
-                        'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Build%20Island/noctisadmin.lua'}; --Build Island 
-	[1490130369] = {'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Anomicrazy/script.lua'}; --Anomic
+	[2000151171] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Build%20Island/yz-script.lua',
+	[1490130369] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Anomicrazy/script.lua'; --Anomic
 }
 
-local urls = placeIds[game.GameId] or {'https://pastebin.com/raw/tQZYjYV0'}
-for i, url in ipairs(urls) do
-    print(i, ': ', url)
-    loadstring(game:HttpGet(url))()
-end
+local url = placeIds[game.GameId] or 'https://pastebin.com/raw/tQZYjYV0'
+loadstring(url)()
