34bd58abaaf6f6d630248e9f6f59c2b5ecd6e592
Blackfire <rexmagnusboe@gmail.com>
2025-07-20 16:53:38 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local placeIds = {
-	[2000151171] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Build%20Island/yz-script.lua',
-	[1490130369] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Anomicrazy/script.lua'; --Anomic
+--[[ Build Island ]]    [2000151171] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Build%20Island/yz-script.lua',
+--[[    Anomic    ]]  	[1490130369] = 'https://raw.githubusercontent.com/BlackfireSoon/Ignation-Hub/refs/heads/main/Anomicrazy/script.lua'; 
 }
 
 local url = placeIds[game.GameId] or 'https://pastebin.com/raw/tQZYjYV0'
