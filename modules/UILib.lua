2da59de5466cd36e5a6e6d882f865c4384f4589f
Blackfire <rexmagnusboe@gmail.com>
2026-02-22 13:48:13 +0100

    Update UILib.lua

--- modules/UILib.lua
+++ modules/UILib.lua
@@ -1,10 +1,5 @@
 --[=[
- d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
-88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
-88      88    88    88            odD'      88      88    88 88ooo88 
-88  ooo 88    88    88          .88'        88      88    88 88~~~88 
-88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
- Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
+IGNATION UI LIB- ALL CREDITS TO BLACKFIRE
 ]=]
 
 local request = (syn and syn.request) or (http and http.request) or http_request or request
@@ -631,7 +626,7 @@ G2L["3b"]["CornerRadius"] = UDim.new(0, 4);
 local Lib = {}
 
 -- StarterGui.IgnGui.IgnModule
-local function C_2()
+function C_2()
 local script = G2L["2"];
 	
 	
